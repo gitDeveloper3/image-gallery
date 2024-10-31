7ab1893f192a04832147bd553b80577db9a2aed9
jhoracekaratu <jhorace.karatu@gmail.com>
2024-10-31 20:57:57 +0300

    add aatribution to img

--- src/app/gallery/PhotoCard.tsx
+++ src/app/gallery/PhotoCard.tsx
@@ -1,9 +1,17 @@
 "use client";
-import { Card, CardContent, Typography, Button, Box, Snackbar, CircularProgress } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  Box,
+  Snackbar,
+  CircularProgress,
+} from "@mui/material";
 import { useState } from "react";
 import { Photo } from "@/schemas/photoSchema"; // Ensure you have the correct import for Photo type
 import { deletePhotoAction } from "@/actions/photoActions";
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 interface PhotoCardProps {
   photo: Photo & { _id: string }; // Ensure _id is included in the photo prop
@@ -16,7 +24,7 @@ export default function PhotoCard({ photo, onActionComplete }: PhotoCardProps) {
   const [success, setSuccess] = useState(false);
 
   const handleDeletePhoto = async (dbId: string, gDriveUrl: string) => {
-    const gDriveId = gDriveUrl.split('id=')[1];
+    const gDriveId = gDriveUrl.split("id=")[1];
 
     setLoading(true); // Set loading to true when starting deletion
     try {
@@ -24,7 +32,7 @@ export default function PhotoCard({ photo, onActionComplete }: PhotoCardProps) {
       setSuccess(true); // Set success state if deletion is successful
       onActionComplete(); // Notify parent component
     } catch (error) {
-      console.log(error)
+      console.log(error);
       // Optionally handle error (e.g., show a notification)
     } finally {
       setLoading(false); // Reset loading state
@@ -33,8 +41,10 @@ export default function PhotoCard({ photo, onActionComplete }: PhotoCardProps) {
 
   // Load the image by setting the src from the data-src only for the clicked photo
   const handleLoadImage = (id: string) => {
-    const gDriveId = photo.url.split('id=')[1];
-    const imgElement = document.getElementById(`img-${id}`) as HTMLImageElement | null;
+    const gDriveId = photo.url.split("id=")[1];
+    const imgElement = document.getElementById(
+      `img-${id}`
+    ) as HTMLImageElement | null;
 
     if (imgElement) {
       imgElement.src = `https://drive.google.com/thumbnail?id=${gDriveId}`; // Set src to data-src
@@ -44,18 +54,32 @@ export default function PhotoCard({ photo, onActionComplete }: PhotoCardProps) {
 
   // Function to copy image tag to clipboard
   const copyToClipboard = () => {
-    const gDriveId = photo.url.split('id=')[1];
-    const imgTag = `<img src="https://drive.google.com/uc?export=view&id=${gDriveId}" caption="${photo.attributes?.caption || ''}" alt="${photo.name}" />`;
-    
-    navigator.clipboard.writeText(imgTag).then(() => {
-      setSuccess(true); // Show success message or notification
-    }).catch(err => {
-      console.error('Failed to copy: ', err); // Handle the error case
-    });
+    const gDriveId = photo.url.split("id=")[1];
+    const imgTag = `<img src="https://drive.google.com/uc?export=view&id=${gDriveId}" caption="${
+      photo.attributes?.caption || ""
+    }" alt="${photo.name}" 
+    attribution="${photo.attribution}"
+    />`;
+
+    navigator.clipboard
+      .writeText(imgTag)
+      .then(() => {
+        setSuccess(true); // Show success message or notification
+      })
+      .catch((err) => {
+        console.error("Failed to copy: ", err); // Handle the error case
+      });
   };
 
   return (
-    <Card sx={{ position: "relative", overflow: "hidden", boxShadow: 3, minWidth: 280 }}>
+    <Card
+      sx={{
+        position: "relative",
+        overflow: "hidden",
+        boxShadow: 3,
+        minWidth: 280,
+      }}
+    >
       {!imageLoaded && (
         <Box
           sx={{
@@ -74,46 +98,63 @@ export default function PhotoCard({ photo, onActionComplete }: PhotoCardProps) {
 
       <img
         id={`img-${photo._id}`}
-        src='https://dummyjson.com/image/150'
+        src="https://dummyjson.com/image/150"
         data-src={photo.url.trim()} // Store actual URL in data-src
         alt={photo.name}
         style={{ height: 200, display: imageLoaded ? "block" : "none" }}
       />
 
       <CardContent>
-        <Typography variant="subtitle1"><strong>Photo Name:</strong> {photo.name}</Typography>
-        <Typography variant="body2"><strong>Caption:</strong> {photo.attributes?.caption || "No caption available"}</Typography>
-        <Typography variant="body2"><strong>Uploaded by:</strong> {photo.attributes?.uploadedBy || "Unknown"}</Typography>
+        <Typography variant="subtitle1">
+          <strong>Photo Name:</strong> {photo.name}
+        </Typography>
         <Typography variant="body2">
-          <strong>Created at:</strong> {new Date(photo.attributes?.createdAt ?? Date.now()).toLocaleDateString()}
+          <strong>Caption:</strong>{" "}
+          {photo.attributes?.caption || "No caption available"}
+        </Typography>
+        <Typography variant="body2">
+          <strong>Uploaded by:</strong>{" "}
+          {photo.attributes?.uploadedBy || "Unknown"}
+        </Typography>
+        <Typography variant="body2">
+          <strong>Created at:</strong>{" "}
+          {new Date(
+            photo.attributes?.createdAt ?? Date.now()
+          ).toLocaleDateString()}
         </Typography>
 
         {/* Display the attribution */}
         <Typography variant="body2">
-          <strong>Attribution:</strong> {photo.attribution || "No attribution provided"}
+          <strong>Attribution:</strong>{" "}
+          {photo.attribution || "No attribution provided"}
         </Typography>
 
         {/* Display the tags */}
         <Typography variant="body2" sx={{ marginTop: 1 }}>
-          <strong>Tags:</strong> {photo.tags.length > 0 ? photo.tags.join(', ') : "No tags available"}
+          <strong>Tags:</strong>{" "}
+          {photo.tags.length > 0 ? photo.tags.join(", ") : "No tags available"}
         </Typography>
 
         {/* Button container with equal sizing */}
-        <Box display="flex" justifyContent="space-between" alignItems="center" marginTop={2}>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          marginTop={2}
+        >
           {/* Button to copy image tag to clipboard */}
-          <Button 
-            variant="contained" 
-            color="primary" 
+          <Button
+            variant="contained"
+            color="primary"
             sx={{ flex: 1, marginRight: 1 }} // Flex sizing with right margin
             onClick={copyToClipboard}
             startIcon={<ContentCopyIcon />} // Adding the icon
+          />
 
-         />
-          
           {/* Delete button */}
-          <Button 
-            variant="contained" 
-            color="secondary" 
+          <Button
+            variant="contained"
+            color="secondary"
             sx={{ flex: 1 }} // Flex sizing
             onClick={() => handleDeletePhoto(photo._id, photo.url)}
             disabled={loading} // Disable button while loading
